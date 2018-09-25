e7a37e99fdfb3ba238578611c8d73ac32b4ef710
chi <csyckad@gmail.com>
2018-09-25 13:16:19 +0800

    Enable logging in the downloadmiddleware

--- hkexnews_scrapy/middlewares.py
+++ hkexnews_scrapy/middlewares.py
@@ -9,6 +9,9 @@ from scrapy import signals
 from selenium import webdriver
 from scrapy.http import HtmlResponse
 from scrapy.exceptions import IgnoreRequest
+import logging
+
+logger = logging.getLogger(__name__)
 
 class SeleniumMiddleware(object):
     def __init__(self,path, option):
@@ -35,11 +38,28 @@ class SeleniumMiddleware(object):
             self.driver.find_element_by_id('btnSearch').click()
             self.driver.switch_to_alert().accept()
             
+            
+
+            
         except:
-            body = self.driver.page_source
-            return HtmlResponse(url=self.driver.current_url, body=body, encoding='utf-8',request = request)
+            try: #If return date not match that date we request, raise exception
+                tmp_date = self.driver.find_element_by_xpath('//*[@id="pnlResult"]/div').text
+                tmp_date= tmp_date.rsplit(' ',1)[1]
+                tmp_date_l = tmp_date.split('/')
+                tmp_date_l.reverse()
+                tmp_date = "".join(tmp_date_l)
+                print("temp date {}".format(tmp_date))
+                if not tmp_date == date:
+                    raise ValueError('Not able to choose on date')
+            except:
+                logger.info("Not able to choose on date %s due to no record" % date)
+                raise IgnoreRequest()
+            else:
+                body = self.driver.page_source
+                return HtmlResponse(url=self.driver.current_url, body=body, encoding='utf-8',request = request)
         else:
-            raise IgnoreRequest("not able to choose that day")
+            logger.info("Not able to choose on date %s due to HKEX not allowed" % date)
+            raise IgnoreRequest()
             
                    
         
@@ -49,105 +69,13 @@ class SeleniumMiddleware(object):
     def from_crawler(cls, crawler):
         path = crawler.settings.get('CHROMEDRIVER_PATH')
         option = crawler.settings.get('CHROME_HEADLESS')
-        return cls(path, option)   
-
-   
-
-
-    def process_exception(self, request, exception, spider):
-        return HtmlResponse(url=self.driver.current_url,status=404)
-
-
-class HkexnewsScrapySpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
+        s = cls(path, option)   
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Response, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
+        return s 
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class HkexnewsScrapyDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
-
     def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        return HtmlResponse(url=self.driver.current_url, status=404)
